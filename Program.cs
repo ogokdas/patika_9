b04d572f7d48de322aaacac0b022e9aea06bab14
ogokdas <ogokdas@yandex.com>
2022-02-04 19:07:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patikaHomework9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = new int[100];
+
+            for(int i =0; i < 100; i++) // Dizi elemanları verdiğim kurala göre dolduruluyor.
+            {
+                dizi[i] = i * 5;
+            }
+
+            foreach(int item in dizi)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
